47b4541b4116b83bdb93f94dccb41224f4fe61ac
thomascr9 <44042018+thomascr9@users.noreply.github.com>
2020-04-26 15:36:23 -0600

    SVM code

--- svm.R
+++ svm.R
@@ -9,25 +9,26 @@ final_data <- final_data %>%
     is_switcher == TRUE ~ 1,
     is_switcher == FALSE ~ 0
   )) %>%
-  select(., is_switcher, low_ses, no_belief, no_home_supp) 
+  select(., is_switcher, fath_educ_scale, moth_educ_scale, work_scale,
+         weak_scale, no_homesupp_scale, no_conf_tf) 
 
 final_data$is_switcher = factor(final_data$is_switcher, levels = c(0,1))
 
-
+install.packages('e1071')
 install.packages('caTools')
 library(caTools)
+library(e1071)
 
+set.seed(472)
 split <- sample.split(final_data$is_switcher, SplitRatio = 0.75)
 training_set <- subset(final_data, split == TRUE) 
 test_set <- subset(final_data, split == FALSE)
 
-install.packages('e1071')
-library(e1071)
+
 
 classifier <- svm(formula = is_switcher ~ .,
                   data = training_set,
-                  kernel = 'radial', 
-                  class.weights= c("0" = 1, "1" = 3))
+                  kernel = 'radial')
 
 classifier
 
@@ -39,14 +40,3 @@ cm <- as.data.frame(table(test_set[,1], y_pred))
 colnames(cm) <- c("Actual Classification", "Prediction", "Frequency")
 formattable(cm, align = "l")
 
-svm_tune <- tune(svm, is_switcher ~ ., data = training_set,
-                 ranges = list(epsilon = seq(0,1,0.01), cost = 2^(2:9),
-                               class.weights = c("0" = 1, "1" = 2))
-)
-print(svm_tune)
-
-best_mod <- svm_tune$best.model
-best_mod_pred <- predict(best_mod, test_set[,-1])
-
-best_cm <- table(test_set[,1],best_mod_pred)
-best_cm
